83ae6d1abb06065447a0524b6a00f34d3825b0ce
silvana <silvana.george@ns.nl>
2024-07-30 10:54:08 +0200

    ChangeCaseDemo

---
+++ src/ChangeCaseDemo.java
@@ -0,0 +1,14 @@
+import java.util.Locale;
+
+public class ChangeCaseDemo {
+
+    public static void main(String[] args) {
+        String originalString = "Java";
+        String upperCaseString = originalString.toUpperCase(Locale.GERMAN);
+        String lowerCaseString = originalString.toLowerCase();
+
+        String specialString = "Java1@Best";
+        String specialUpperCaseString = specialString.toUpperCase();
+        String specialLowerCaseString = specialString.toLowerCase();
+    }
+}
