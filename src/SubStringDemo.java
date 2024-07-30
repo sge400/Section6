4708494ab6c6f04164ccb3c13cd47a7ac49c6a16
silvana <silvana.george@ns.nl>
2024-07-30 11:45:32 +0200

    TrimDemo

---
+++ src/SubStringDemo.java
@@ -0,0 +1,8 @@
+public class SubStringDemo {
+
+    public static void main(String[] args) {
+        String originalString = "Hello, World!";
+        String subString1 = originalString.substring(7);
+        String subString2 = originalString.substring(0, 5); //endIndex is not included!
+    }
+}
