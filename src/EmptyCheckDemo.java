1704ae85704089f68b67850fde56cd79db73e324
silvana <silvana.george@ns.nl>
2024-07-30 10:37:04 +0200

    EmptyCheckDemo

---
+++ src/EmptyCheckDemo.java
@@ -0,0 +1,17 @@
+public class EmptyCheckDemo {
+
+    public static void main(String[] args) {
+        String myString = "";
+        boolean isEmpty = myString.isEmpty();
+        boolean isLengthZero = myString.length() == 0;
+        boolean isEqualEmpty = "".equals(myString);
+
+        String nullString = null;
+        boolean isNullEmpty = "".equals(nullString);
+        //boolean isEmptyNull = nullString.isEmpty();
+        //boolean isEmptyLength = nullString.length() == 0;
+        String blankString = "     "; //not empty, has whitespace
+        boolean isBlankEmpty = blankString.isEmpty();
+        boolean isBlank = blankString.isBlank();
+    }
+}
