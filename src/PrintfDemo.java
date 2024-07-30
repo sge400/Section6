a04df1038861251130a7f4bf5d706b99a5c75a56
silvana <silvana.george@ns.nl>
2024-07-30 14:18:41 +0200

    StringFormatDemo

---
+++ src/PrintfDemo.java
@@ -0,0 +1,12 @@
+public class PrintfDemo {
+
+    public static void main(String[] args) {
+//        String result = String.format("Name: %s, Age: %d", "John", 25);
+//        System.out.println(result);
+
+        System.out.printf("Name: %s, Age: %d%n", "John", 25);
+        System.out.printf("Price: %.2f%n", 19.99);
+        System.out.printf("Grade: %c%n", 'A');
+        System.out.printf("Is Java fun?: %b%n", true);
+    }
+}
