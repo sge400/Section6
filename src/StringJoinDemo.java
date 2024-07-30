ef3cf58d64661057e7976806e83bc316f93940ce
silvana <silvana.george@ns.nl>
2024-07-30 13:39:31 +0200

    StringJoinDemo

---
+++ src/StringJoinDemo.java
@@ -0,0 +1,7 @@
+public class StringJoinDemo {
+
+    public static void main(String[] args) {
+        String result = String.join(",", "Hello", "World", "Java");
+        String result1 = String.join("", "Java is", "fun and", "powerfull");
+    }
+}
