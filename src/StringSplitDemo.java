a2540b29a6761ee11420e1714ff1aed69f9cf4a3
silvana <silvana.george@ns.nl>
2024-07-30 13:30:38 +0200

    StringSplitDemo

---
+++ src/StringSplitDemo.java
@@ -0,0 +1,7 @@
+public class StringSplitDemo {
+
+    public static void main(String[] args) {
+        String fruits = "apple,orange,banana,grape";
+        String [] fruitArray = fruits.split(",");
+    }
+}
