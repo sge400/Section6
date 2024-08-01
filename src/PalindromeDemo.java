013bc7ab553d1f999ff965ef72fbf3a0acb091a7
silvana <silvana.george@ns.nl>
2024-08-01 12:04:26 +0200

    ParsingDemo

--- src/PalindromeDemo.java
+++ src/PalindromeDemo.java
@@ -2,11 +2,13 @@ public class PalindromeDemo {
 
     public static void main(String[] args) {
         String input = "mom";
-        StringBuilder sb = new StringBuilder();
+        StringBuilder sb = new StringBuilder(); //or StringBuffer
         sb.append(input);
         sb.reverse();
         String output = sb.toString();
 
         boolean isPalindrome = input.equals(output);
+
+        System.out.printf("The word %s is a palindrome: %b", input, isPalindrome);
     }
 }
--- src/StringMutableDemo.java
+++ src/StringMutableDemo.java
@@ -1,7 +1,7 @@
 import java.io.CharArrayWriter;
 import java.io.IOException;
 
-public class MutableStringsDemo {
+public class StringMutableDemo {
 
     public static void main(String[] args) throws IOException {
 
