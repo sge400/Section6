bddf2ff7ba36bad293dbe9cdebe22f55b4940ec5
silvana <silvana.george@ns.nl>
2024-08-28 17:34:54 +0200

    Section7 finish LogicalOperatorsDemo

---
+++ src/operatorOperand/LogicalOperatorsDemo.java
@@ -0,0 +1,25 @@
+package operatorOperand;
+
+public class LogicalOperatorsDemo {
+
+    public static void main(String[] args) {
+
+        boolean isValid;
+        isValid = !true;
+        isValid = !false;
+        int num1 = 9;
+        int num2 = 6;
+        isValid = !(num1>num2);
+
+        boolean a = (num1>10) & ((num2=16)>15);
+        System.out.println(a);
+        System.out.println(num2);
+
+        boolean b = (num1 < 10) | ((num2=18)>15);
+        System.out.println(b);
+        System.out.println(num2);
+
+        boolean c = (num1>5)^(num2>10);
+        System.out.println(c);
+    }
+}
