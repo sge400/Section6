d1f0712d7dddc0901bcfb320fc76bc00a714a17e
silvana <silvana.george@ns.nl>
2024-08-29 14:48:48 +0200

    Section8 if-else statements, nested if else statements

---
+++ src/controlFlowStatements/FindTheDay.java
@@ -0,0 +1,36 @@
+package controlFlowStatements;
+
+public class FindTheDay {
+
+    public static void main(String[] args) {
+        String whatIsToday = "Saturday";
+
+//        if (whatIsToday.equals("Monday")) {
+//            System.out.println("It is a week day");
+//        } else if (whatIsToday.equals("Tuesday")){
+//            System.out.println("It is a week day");
+//        } else if (whatIsToday.equals("Wednesday")) {
+//            System.out.println("It is a week day");
+//        } else if (whatIsToday.equals("Thursday")) {
+//            System.out.println("It is a week day");
+//        } else if (whatIsToday.equals("Friday")) {
+//            System.out.println("It is a week day");
+//        } else if (whatIsToday.equals("Saturday")) {
+//            System.out.println("It is weekend");
+//        } else if (whatIsToday.equals("Sunday")) {
+//            System.out.println("It is weekend");
+//        } else {
+//            System.out.println("Invalid input please check the day");
+//        }
+
+        if (whatIsToday.equals("Monday") || whatIsToday.equals("Tuesday")
+                || whatIsToday.equals("Wednesday") || whatIsToday.equals("Thursday")
+                || whatIsToday.equals("Friday")) {
+                System.out.println("It is a week day");
+                } else if (whatIsToday.equals("Saturday") || whatIsToday.equals("Sunday")) {
+            System.out.println("It is weekend");
+        } else {
+            System.out.println("Invalid input please check the day");
+        }
+    }
+}
---
+++ src/controlFlowStatements/MessageForDay.java
@@ -0,0 +1,26 @@
+package controlFlowStatements;
+
+public class MessageForDay {
+
+    public static void main(String[] args) {
+
+        String dayOfWeek = "Wednesday";
+
+        if (dayOfWeek.equals("Saturday") || dayOfWeek.equals("Sunday")) {
+            System.out.println("Hooray, it's the weekend!");
+            if (dayOfWeek.equals("Saturday")) {
+                System.out.println("Time for a relaxing day of maybe some outdoor activities!");
+            } else {
+                System.out.println("Lazy Sunday vibes perfect for a cozy day indoors.");
+            }
+        } else {
+            System.out.println("It's a week day. Time to work or attend classes.");
+            if (dayOfWeek.equals("Wednesday") || dayOfWeek.equals("Thursday") || dayOfWeek.equals("Friday")) {
+                System.out.println("Midweek hustle! Keep going, the weekend is approaching.");
+            } else {
+                System.out.println("Monday blues? Grab some coffee and power through the day!");
+            }
+        }
+    }
+
+}
