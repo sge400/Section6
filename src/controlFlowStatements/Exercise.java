4bd619c97a9905a34b139e8b9a93cf0a9f9701da
silvana <silvana.george@ns.nl>
2024-09-02 10:26:59 +0200

    Section8 exercise while loop

---
+++ src/controlFlowStatements/Exercise.java
@@ -0,0 +1,21 @@
+package controlFlowStatements;
+
+public class Exercise {
+
+    public static int findDivisibleNumber (int num1, int num2) {
+        int i =1;
+
+        while (true) { //as long as the condition = true, the loop runs.
+            if (i% num1 ==0 && i% num2 ==0) {
+                break;
+            } else {
+                i++;
+
+            }
+        }
+        System.out.println(i);
+        return i;
+
+    }
+
+}
---
+++ src/controlFlowStatements/ExerciseMain.java
@@ -0,0 +1,8 @@
+package controlFlowStatements;
+
+public class ExerciseMain {
+    public static void main(String[] args) {
+        Exercise.findDivisibleNumber(2,3);
+        Exercise.findDivisibleNumber(2,5);
+    }
+}
