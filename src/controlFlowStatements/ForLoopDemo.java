7a627595c2c7090a38a3f843355cfdba220030fd
silvana <silvana.george@ns.nl>
2024-09-02 13:35:00 +0200

    Section8 for loops

---
+++ src/controlFlowStatements/ForLoopDemo.java
@@ -0,0 +1,18 @@
+package controlFlowStatements;
+
+public class ForLoopDemo {
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 10; i++) {
+            int result = i * 5;
+            System.out.println("5 * " + i + " = " + result);
+        }
+
+//        for (; ;){
+//            System.out.println("This is an infinite for loop!");
+//        }
+
+//        for (;;); //another for loop
+    }
+}
