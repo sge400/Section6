cf4580890ba0a8d186292b469b6c34b9cd4f8fb2
silvana <silvana.george@ns.nl>
2024-09-03 15:16:13 +0200

    Section10 dynamic poly demo

---
+++ src/inheritance/marvel/app/DynamicPoly.java
@@ -0,0 +1,12 @@
+package inheritance.marvel.app;
+
+import inheritance.marvel.base.Person;
+import inheritance.marvel.heroes.IronMan;
+
+public class DynamicPoly {
+
+    public static void main(String[] args) {
+        Person person = new IronMan(); //upcasting
+        person.walk(); //from Person class or IronMan class? -> IronMan!
+    }
+}
--- src/inheritance/marvel/heroes/IronMan.java
+++ src/inheritance/marvel/heroes/IronMan.java
@@ -8,5 +8,9 @@ public class IronMan extends Person {
         System.out.println("IronMan is using his power");
     }
 
+    @Override
+    public void walk(){ //method override with same method name, return type and parameters
+        System.out.println("IronMan walking with his suit"); //access modifier can't be more restrictive than original method
+    }
 
 }
