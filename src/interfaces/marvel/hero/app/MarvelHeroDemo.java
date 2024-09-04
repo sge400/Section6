5e3703daa5b13d37825cf3ea3cbfc783d5328b0a
silvana <silvana.george@ns.nl>
2024-09-04 13:49:24 +0200

    Section11 advantages of interfaces

---
+++ src/interfaces/marvel/hero/app/MarvelHeroDemo.java
@@ -0,0 +1,28 @@
+package interfaces.marvel.hero.app;
+
+import interfaces.marvel.hero.Superhero;
+import interfaces.marvel.hero.impl.CaptainAmerica;
+import interfaces.marvel.hero.impl.IronMan;
+import interfaces.marvel.hero.impl.SpiderMan;
+
+public class MarvelHeroDemo {
+
+    public static void main(String[] args) {
+
+        Superhero ironMan = new IronMan();
+        /*System.out.println(ironMan.usePower());
+        System.out.println(ironMan.stopVillain('N'));*/
+        invokeSuperHero(ironMan);
+
+        Superhero spiderMan = new SpiderMan();
+        invokeSuperHero(spiderMan);
+
+        Superhero captainAmerica = new CaptainAmerica();
+        invokeSuperHero(captainAmerica);
+    }
+
+    private static void invokeSuperHero(Superhero superhero) {
+        System.out.println(superhero.usePower());
+        System.out.println(superhero.stopVillain('N'));
+    }
+}
