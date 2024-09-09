4a3aaea7d12900d0c9d626562ee078fbfb51d815
silvana <silvana.george@ns.nl>
2024-09-09 13:25:36 +0200

    Section12 ToStringDemo

---
+++ src/arrays/main/ToStringDemo.java
@@ -0,0 +1,12 @@
+package arrays.main;
+
+import java.util.Arrays;
+
+public class ToStringDemo {
+
+    public static void main(String[] args) {
+        int[] numbers = {1,2,3,4,5};
+        System.out.println(Arrays.toString(numbers));
+        System.out.println(numbers);
+    }
+}
