6690f1afdaef1b58f383b462d572d9d8dded9cac
silvana <silvana.george@ns.nl>
2024-09-09 12:28:08 +0200

    Section12 ArrayForEachLoopDemo

---
+++ src/arrays/main/ArrayForEachLoopDemo.java
@@ -0,0 +1,25 @@
+package arrays.main;
+
+public class ArrayForEachLoopDemo {
+
+    public static void main(String[] args) {
+        int[] numbers = {1,2,3,4,5};
+
+        for(int num : numbers) {
+            System.out.println(num);
+        }
+
+        String[] names = {"Madan", "John", "Lucy"};
+
+        for(String name : names) {
+            System.out.println(name);
+        }
+
+        //scenario where for each loop can't be used
+        for(int i = 0; i < names.length; i++) {
+            names[i] = names[i].toUpperCase();
+            System.out.println(names[i]);
+
+        }
+    }
+}
