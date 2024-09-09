be9e5a310b1e761cc81537404de80072b50c82e3
silvana <silvana.george@ns.nl>
2024-09-09 10:05:16 +0200

    Section12 ArraysBasics

---
+++ src/arrays/main/ArraysBasics.java
@@ -0,0 +1,12 @@
+package arrays.main;
+
+public class ArraysBasics {
+
+    public static void main(String[] args) {
+        int mobileNumber1, mobileNumber2, mobileNumber3;
+
+        int[] mobileNumbers;
+    }
+}
+
+
