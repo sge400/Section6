118e4fced7b65c4bd766f40ff00ee5db68eade8d
silvana <silvana.george@ns.nl>
2024-09-09 10:21:17 +0200

    Section12 declaring and initializing arrays

--- src/arrays/main/ArraysBasics.java
+++ src/arrays/main/ArraysBasics.java
@@ -1,11 +1,27 @@
 package arrays.main;
 
+import arrays.model.Person;
+
 public class ArraysBasics {
 
     public static void main(String[] args) {
         int mobileNumber1, mobileNumber2, mobileNumber3;
 
-        int[] mobileNumbers;
+        int[] mobileNumbers = new int[5];
+        mobileNumbers[0] = 766554433;
+        mobileNumbers[1] = 566554433;
+        mobileNumbers[2] = 567554433;
+        mobileNumbers[3] = 766573233;
+        mobileNumbers[4] = 766738433;
+        int[] nums = {1,2,3,4,5};
+        int mobileNums[]; //2 different ways of declaring arrays
+
+        double[] prices;
+        char[] grades;
+        String[] names;
+
+        int length = 100;
+        Person[] persons = new Person[length*2];
     }
 }
 
---
+++ src/arrays/model/Person.java
@@ -0,0 +1,4 @@
+package arrays.model;
+
+public class Person {
+}
