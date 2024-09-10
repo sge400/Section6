6c6a57a21a8f66dd2af7173cc9e8f359f47cca1c
silvana <silvana.george@ns.nl>
2024-09-10 14:56:56 +0200

    Section14 finally block

--- src/exception/ExceptionDemo.java
+++ src/exception/ExceptionDemo.java
@@ -6,15 +6,20 @@ import java.util.Scanner;
 public class ExceptionDemo {
 
     public static void main(String[] args) {
+        Scanner scanner = null;
         try {
-            Scanner scanner = new Scanner(System.in);
+            scanner = new Scanner(System.in);
             System.out.println("Enter a number...");
             int number = scanner.nextInt();
             System.out.println(number);
-            scanner.close();
-
+            //scanner.close(); //will not be executed in case of an exception and your connection will never be closed
         } catch (Exception ex) {
             System.out.println("Please provide input in numerical format only and try again...");
+        } finally {
+            System.out.println("finally block is being executed");
+            if(scanner != null) {
+                scanner.close();
+            }
         }
 
     }
