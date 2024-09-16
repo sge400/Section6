1087219b1227c6ab609bd385bc9e7eb639e9b168
silvana <silvana.george@ns.nl>
2024-09-16 11:05:26 +0200

    Section 15 HashCodeEqualsDemo

--- src/object/demo/Person.java
+++ src/object/demo/Person.java
@@ -1,4 +1,50 @@
 package object.demo;
 
-public class Person {
+public class Person { //POJO class
+
+    private String name;
+    private int age;
+    private char gender;
+    private int ssn;
+
+    public Person (){}
+
+    public Person(String name, int age, char gender, int ssn) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.ssn = ssn;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public void setGender(char gender) {
+        this.gender = gender;
+    }
+
+    public int getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(int ssn) {
+        this.ssn = ssn;
+    }
 }
