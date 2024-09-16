b66afb76dec39b47cb1be4029f98122013652daf
silvana <silvana.george@ns.nl>
2024-09-16 13:37:38 +0200

    Section 15 deep vs shallow cloning

---
+++ src/object/demo/CloneExample.java
@@ -0,0 +1,14 @@
+package object.demo;
+
+public class CloneExample {
+
+    public static void main(String[] args) throws CloneNotSupportedException {
+        Course course = new Course();
+        course.setCourseName("Math");
+
+        Student originalStudent = new Student("Alice", course);
+        Student clonedStudent = (Student) originalStudent.clone();
+        System.out.println(originalStudent == clonedStudent); //false
+        System.out.println(originalStudent.getCourse() == clonedStudent.getCourse()); //true for shallow cloning, false for deep
+    }
+}
