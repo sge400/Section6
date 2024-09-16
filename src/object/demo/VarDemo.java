0af1ad7b2da86be8fe08d48b0152bd0c5530bf93
silvana <silvana.george@ns.nl>
2024-09-16 14:32:53 +0200

    Section 15 var local variables

---
+++ src/object/demo/VarDemo.java
@@ -0,0 +1,10 @@
+package object.demo;
+
+public class VarDemo {
+
+    public static void main(String[] args) {
+        var message = "Hello World!"; //always initialize
+        var person = new Person();
+        var arr = new ArrayIndexOutOfBoundsException();
+    }
+}
