ae327f7d9a224f499c410e450dabd2d3349804a3
silvana <silvana.george@ns.nl>
2024-09-16 14:20:53 +0200

    Section 15 record classes

--- src/object/demo/RecordDemo.java
+++ src/object/demo/RecordDemo.java
@@ -1,5 +1,7 @@
 package object.demo;
 
+import java.lang.reflect.RecordComponent;
+
 public class RecordDemo {
 
     public static void main(String[] args) {
@@ -8,5 +10,10 @@ public class RecordDemo {
         System.out.println(personRecord.occupation());
         System.out.println(personRecord);
         Class personClass = personRecord.getClass();
+        System.out.println(personClass.isRecord());
+        RecordComponent[] recordComponents = personClass.getRecordComponents();
+        for (RecordComponent r : recordComponents) {
+            System.out.println(r.toString());
+        }
     }
 }
