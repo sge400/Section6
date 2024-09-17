33ad9583e013e6f129ba52cd6da2fa7947f3197f
silvana <silvana.george@ns.nl>
2024-09-17 11:08:43 +0200

    Section 17 diamond operator

---
+++ src/list/DiamondOperator.java
@@ -0,0 +1,15 @@
+package list;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DiamondOperator {
+
+    public static void main(String[] args) {
+        //DRY - Don't repeat yourself
+        ArrayList<Integer> nums = new ArrayList<>(); //diamond operator syntax
+        List<Character> characters = new ArrayList<>();
+        var doubleNums = new ArrayList<Double>();  //if you use var, then you need to specify datatype on right side
+        List<String> countryNames = new ArrayList<>();
+    }
+}
