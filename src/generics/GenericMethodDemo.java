95b11d000eabec7263968c7359b4710b46f5113f
silvana <silvana.george@ns.nl>
2024-09-18 16:35:01 +0200

    Section 19 generic methods

---
+++ src/generics/GenericMethodDemo.java
@@ -0,0 +1,17 @@
+package generics;
+
+public class GenericMethodDemo {
+
+    public static void main(String[] args) {
+        String[] stringArray = {"Hello", "World"};
+        printArray(stringArray);
+        Integer[] intArray = {1, 2, 3, 4, 5};
+        printArray(intArray);
+    }
+
+    public static <T> void printArray(T[] array) {
+        for (T element : array) {
+            System.out.println(element);
+        }
+    }
+}
