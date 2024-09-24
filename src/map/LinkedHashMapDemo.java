665170eba9b01c0d94ba2d65730ba769566b28a5
silvana <silvana.george@ns.nl>
2024-09-24 13:49:54 +0200

    Section 20 Linked Hash Map

---
+++ src/map/LinkedHashMapDemo.java
@@ -0,0 +1,26 @@
+package map;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.SequencedMap;
+
+public class LinkedHashMapDemo {
+
+    public static void main(String[] args) {
+        //creating a LinkedHashMap
+        LinkedHashMap<Integer, String> linkedHashMap = new LinkedHashMap<>();
+
+        linkedHashMap.put(4, "Four");
+        linkedHashMap.put(1, "One");
+        linkedHashMap.put(7, "Seven");
+        linkedHashMap.put(2, "Two");
+
+        for(Map.Entry<Integer, String> entry : linkedHashMap.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
+        SequencedMap<Integer, String> reversedLinkedHashMap = linkedHashMap.reversed();
+        for(Map.Entry<Integer, String> entry : reversedLinkedHashMap.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
+    }
+}
