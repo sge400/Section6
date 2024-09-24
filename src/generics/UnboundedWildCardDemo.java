7448a6fc4f0c92772f984e5b4f4149d859133e88
silvana <silvana.george@ns.nl>
2024-09-24 10:38:09 +0200

    Section 19 Unbounded WildCards

---
+++ src/generics/UnboundedWildCardDemo.java
@@ -0,0 +1,23 @@
+package generics;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class UnboundedWildCardDemo {
+
+    public static void main(String[] args) {
+        List<Integer> li = Arrays.asList(1,2,3);
+        List<String> ls = Arrays.asList("One", "Two", "Three");
+        printList(li);
+        printList(ls);
+
+    }
+
+    public static void printList(List<?> list) {
+        for(Object obj : list) {
+            System.out.println(obj);
+        }
+        System.out.println(list.size());
+    }
+}
