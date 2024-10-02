5fab6caf9b6c296775ca3551dc5b7d32b3ec5372
silvana <silvana.george@ns.nl>
2024-10-02 12:53:53 +0200

    Section 6 Palindrome exercise

--- src/strings/PalindromeDemo.java
+++ src/strings/PalindromeDemo.java
@@ -5,12 +5,10 @@ public class PalindromeDemo {
     public static void main(String[] args) {
         String input = "mom";
         StringBuilder sb = new StringBuilder(2); //or StringBuffer
-        sb.append(input);
-        sb.reverse();
-        String output = sb.toString();
+        String output = sb.append(input).reverse().toString();
 
         boolean isPalindrome = input.equals(output);
 
-        System.out.printf("The word %s is a palindrome: %b\n", input, isPalindrome);
+        System.out.printf("The word %s is a palindrome: %b%n", input, isPalindrome);
     }
 }
