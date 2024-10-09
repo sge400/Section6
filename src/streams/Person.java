7b0cca79065e8a511a2b184d2910701bd42470ad
silvana <silvana.george@ns.nl>
2024-10-09 11:25:24 +0200

    Section 26 flatMap()

---
+++ src/streams/Person.java
@@ -0,0 +1,22 @@
+package streams;
+
+import java.util.List;
+
+public class Person {
+
+    private String name;
+    private List<String> phoneNumbers;
+
+    public Person(String name, List<String> phoneNumbers) {
+        this.name = name;
+        this.phoneNumbers = phoneNumbers;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<String> getPhoneNumbers() {
+        return phoneNumbers;
+    }
+}
