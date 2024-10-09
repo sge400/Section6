b41990ac98b96cd695f692153e403f7f1b481804
silvana <silvana.george@ns.nl>
2024-10-09 16:17:37 +0200

    Section 27 Optional to deal with nulls

---
+++ src/optional/StreamAPINullCheckTip.java
@@ -0,0 +1,16 @@
+package optional;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+public class StreamAPINullCheckTip {
+
+    public static void main(String[] args) {
+        List<String> countryNames = Arrays.asList("USA", "India", null, "Canada", null, "UK", null, null, "Germany");
+        List<String> capitalNames = countryNames.stream().filter(Objects::nonNull)
+                .map(String::toUpperCase).collect(Collectors.toList());
+        System.out.println(capitalNames);
+    }
+}
