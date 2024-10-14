46740a9adb27381b8a36039a6c8023a28a47faca
silvana <silvana.george@ns.nl>
2024-10-14 14:03:18 +0200

    Section 28 volatile keyword

---
+++ src/multithreading/VolatileExample.java
@@ -0,0 +1,24 @@
+package multithreading;
+
+public class VolatileExample {
+
+    public static void main(String[] args) throws InterruptedException {
+        SharedBooleanResource sharedBooleanResource = new SharedBooleanResource();
+        Thread workerThread = new Thread(()-> sharedBooleanResource.doWork());
+
+
+        Thread stopperThread = new Thread(()-> {
+            try {
+                Thread.sleep(3000);
+                sharedBooleanResource.setStopFlag();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        workerThread.start();
+        stopperThread.start();
+        workerThread.join();
+        stopperThread.join();
+
+    }
+}
