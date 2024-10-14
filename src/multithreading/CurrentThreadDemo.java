6c3be81893f43f5af2ceaa334ccefd0a9b446e91
silvana <silvana.george@ns.nl>
2024-10-14 12:48:55 +0200

    Section 28 thread methods

---
+++ src/multithreading/CurrentThreadDemo.java
@@ -0,0 +1,10 @@
+package multithreading;
+
+public class CurrentThreadDemo {
+
+    public static void main(String[] args) {
+        Thread mainThread = Thread.currentThread();
+        System.out.println(mainThread.threadId());
+        System.out.println(mainThread.getName());
+    }
+}
